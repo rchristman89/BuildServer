335f7412a462cd36a2b3d522c080d49a98dfac3b
rchristman <ryan.christman@microsoft.com>
2019-02-28 21:55:22 -0500

    Updated

--- BuildConfig/BuildConfig.ps1
+++ BuildConfig/BuildConfig.ps1
@@ -1,18 +1,36 @@
-﻿git config --global http.sslverify false
+﻿New-EventLog –LogName Application –Source 'BuildScript'
+
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Configuring git'
+git config --global http.sslverify false
 git config --global credential.helper wincred
 git config --global credential.scmpeoc3t.army.mil.authority negotiate
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Configured git'
 
-Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Installing Pester'
+$null = Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies
 Install-Module Pester -Force -SkipPublisherCheck -Confirm:$false
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Installed Pester'
 
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Updating Help'
 Update-Help -Force -Confirm:$false
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Updated Help'
 
-winrm quickconfig -quiet
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Configuring WinRM'
+$null = winrm quickconfig -quiet
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Configured WinRM'
 
-
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Downloading Package list'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-New-Item 'C:\Repo' -ItemType Directory
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rchristman89/BuildServer/master/packages.config" -OutFile "C:\packages.config"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rchristman89/BuildServer/master/packages.config' -OutFile 'C:\packages.config'
 
-$command = '. "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\ReadyRoll\OctoPack\build\NuGet.exe" install "C:\packages.config" -OutputDirectory "C:\NuGetpackages"'
+until((Test-Path 'C:\Repo\Source\Solutions') -eq $true)
+{
+    Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Waiting for Repo to Clone'
+    Start-Sleep -Seconds 30
+
+}
+
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Performing NuGet Restore'
+$command = '. "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\ReadyRoll\OctoPack\build\NuGet.exe" install "C:\packages.config" -OutputDirectory "C:\Repos\Core\Source\Solutions"'
 Invoke-Expression $command
+Write-EventLog -LogName Application -Source 'BuildScript' -EntryType Information -EventId 1 -Message 'Completed NuGet Restore'
